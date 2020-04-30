c4214a7922f88eca9a147e33c1fd2c0ce91e42d7
Jurek Kurianski <jurek@inventionsource.com.au>
2020-04-30 11:50:13 +0700

    changed to java 13

--- src/main/java/inventionsource/com/au/pidemo/PiDemo.java
+++ src/main/java/inventionsource/com/au/pidemo/PiDemo.java
@@ -15,9 +15,11 @@ public class PiDemo {
 
     private static final Logger log = LogManager.getLogger(PiDemo.class);
 
-    final static String VERSION = "1.01";
+    final static String VERSION = "1.02";
  /*
   Invention Source (c) 2020    
+    final static String VERSION = "1.02"; 2020-04-29 Works on win/lin start again
+                                         WinScp changed to ssh Openssh agent
   final static String VERSION = "1.01"; 2020-04-26 clean up source
   final static String VERSION = "1.00"; 2020-04-21 Linux
  */
@@ -32,24 +34,28 @@ public class PiDemo {
             gpio.removeAllTriggers();
             gpio.removeAllListeners();
             //
-            GpioPinDigitalOutput outYellow = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27,"outYellowPin27",PinState.LOW);
+            GpioPinDigitalOutput outYellow = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27,
+                                                                        "outYellowPin27",PinState.LOW);
             outYellow.clearProperties();
             outYellow.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
             outYellow.low();
             
-            GpioPinDigitalOutput outGreen = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25,"outGreenPin25",PinState.LOW);
+            GpioPinDigitalOutput outGreen = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25,
+                                                                        "outGreenPin25",PinState.LOW);
             outGreen.clearProperties();
             outGreen.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
             outYellow.low();
             
             log.info("Added output pins: " + outYellow.getName() + " : "+ outGreen.getName());
             
-            GpioPinDigitalInput inBlue = gpio.provisionDigitalInputPin(RaspiPin.GPIO_22,"inPin22", PinPullResistance.PULL_UP);
+            GpioPinDigitalInput inBlue = gpio.provisionDigitalInputPin(RaspiPin.GPIO_22,"inPin22",
+                                                                        PinPullResistance.PULL_UP);
             inBlue.setShutdownOptions(true);            
             log.info("Added input pin: " + inBlue.getName() );
             inBlue.setDebounce(1000);
             log.info("setDebounce(1000) on: " + inBlue.getName() );
-            log.info("------------------------- Output/Input Pins setup done. --------------------------------------");
+            log.info(
+              "------------------------- Output/Input Pins setup done. --------------------------------------");
 
             //------------------------------------------------------------------------------
             //------------       GpioPinDigitalStateChange       ---------------------------
@@ -62,27 +68,31 @@ public class PiDemo {
                     log.info("--------------  PinDigitalStateChange --------------");
                     
                     String blueStatus= event.getState().toString();
-                    String blueStatusStamp = (DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss").format(LocalDateTime.now())) +
+                    String blueStatusStamp =
+                            (DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss").format(LocalDateTime.now())) +
                             "-" + blueStatus ; 
                     outGreen.low();
                     outYellow.low();
                     log.info("PinDigitalStateChange now blueStatusStamp: "+ blueStatusStamp);
                        try {
-                            if (blueStatus == "HIGH"){
+                            if (blueStatus.equalsIgnoreCase("HIGH")){
                                 outGreen.high();
                                 outYellow.low();
                             }
-                            if (blueStatus == "LOW"){
+                            if (blueStatus.equalsIgnoreCase("LOW")){
                                 outYellow.high();
                                 outGreen.low();
                            }
-                            if ((blueStatus != "LOW" && blueStatus != "HIGH") ){
-                                log.error("PinDigitalStateChange Error somewhere. blueStatus != LOW && blueStatus != HIGH blueStatus: " );
+                            if (!blueStatus.equalsIgnoreCase("LOW") &&
+                                    !blueStatus.equalsIgnoreCase("HIGH") ){
+                                log.error("PinDigitalStateChange Error somewhere. blueStatus != " +
+                                        "LOW && blueStatus != HIGH. blueStatus: " + blueStatus);
                                 ToggleLed(outYellow, 1000, 10);
                             }
                         } catch (Exception ex) {
                            // Exception toggle outYellow led on error
-                            log.error("Exception Error PinDigitalStateChange blueStatus : " + blueStatus, ex.getMessage());
+                            log.error("Exception Error PinDigitalStateChange blueStatus : " +
+                                                blueStatus, ex.getMessage());
                             ToggleLed(outYellow, 500, 20);
                         } 
                     log.info("------  END  --------  PinDigitalStateChange --------------");
@@ -123,7 +133,8 @@ public class PiDemo {
     }
 
     public static void ToggleLed(GpioPinDigitalOutput outPin, long mills , int count){
-        log.error("outPin: " + outPin.getName()+ " toggle mmills: "+ mills + " count: " + count);
+        log.error("outPin: " + outPin.getName()+ " toggle mmills: "+ mills +
+                        " count: " + count);
         try {
             for (int i=0; i <count; i++){
                 outPin.toggle();
@@ -131,7 +142,8 @@ public class PiDemo {
             }
             outPin.low();        
         } catch (Exception e) {
-            log.error("Error in toggle: " + outPin.getName()+ "  mills: "+ mills + " count: " + count, e);
+            log.error("Error in toggle: " + outPin.getName()+ "  mills: "+ mills +
+                            " count: " + count, e);
         }
     }
 }
